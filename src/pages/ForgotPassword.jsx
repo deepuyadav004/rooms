1a16a3d3b4a7b1d18c9cd121d195d4aae64964a5
Deepu Yadav <deepu.ug20@nsut.ac.in>
2024-03-28 17:05:00 +0530

    complete forgot password page functionality

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 import OAuth from '../components/OAuth';
+import { toast } from 'react-toastify';
+import { getAuth, sendPasswordResetEmail } from 'firebase/auth';
 
 function ForgotPassword() {
 
@@ -8,6 +10,17 @@ function ForgotPassword() {
 
   function onChange(e){
     setEmail(e.target.value)
+  }
+
+  async function onSubmit(e){
+    e.preventDefault()
+    try {
+      const auth = getAuth()
+      await sendPasswordResetEmail(auth, email)
+      toast.success("Email was sent") 
+    } catch (error) {
+      toast.error("Could not send reset password")
+    }
   }
 
   return (
@@ -18,7 +31,7 @@ function ForgotPassword() {
           <img src='https://st.depositphotos.com/1316534/2553/i/450/depositphotos_25538373-Womans-hand-holding-keys-to-new-house.jpg' alt='key' className='w-full rounded-2xl' />
         </div>
         <div className='w-full md:w-[67%] lg:w-[40%] lg:ml-20' >
-          <form>
+          <form onSubmit={onSubmit}>
             <input className='mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out' type='email' id='email' value={email} onChange={onChange} placeholder='Email address' />
             
 
