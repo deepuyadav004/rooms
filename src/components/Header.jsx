7b9f6a9451b7a5f3d07e715eca0f483ec8022404
Deepu Yadav <deepu.ug20@nsut.ac.in>
2024-03-31 16:46:20 +0530

    Hot fixes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { useLocation } from 'react-router'
 import { useNavigate } from 'react-router'
 import {getAuth, onAuthStateChanged} from 'firebase/auth'
-
+import logo from '../../src/assets/Rooms.png'
 function Header() {
 
     const location = useLocation()
@@ -29,7 +29,7 @@ function Header() {
     <div className='bg-white border-b shadow-sm sticky top-0 z-40' >
         <header className='flex justify-between items-center px-3 max-w-6xl mx-auto'>
             <div>
-                <img src='https://private-user-images.githubusercontent.com/77964981/318258252-85a05592-0556-45d8-ba2a-f0c7a4db15f4.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MTE4ODE3ODQsIm5iZiI6MTcxMTg4MTQ4NCwicGF0aCI6Ii83Nzk2NDk4MS8zMTgyNTgyNTItODVhMDU1OTItMDU1Ni00NWQ4LWJhMmEtZjBjN2E0ZGIxNWY0LnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDAzMzElMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwMzMxVDEwMzgwNFomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTllYjRlYzYxNmI3MzU1ZWFhZTk5Y2E0YTllYzYzNzBhOTg2MDhmMjBiOTQ1ODFlODI2NDIxMDhiMjRmMzAxMzgmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.h2Tj7QPHn5LafAma-GD7eY3_WXPr6NErNq_yyhNFQLw' alt='logo' className='h-10 cursor-pointer' onClick={()=>navigate("/")} />
+                <img src={logo} alt='logo' className='h-10 cursor-pointer' onClick={()=>navigate("/")} />
             </div>
             <div>
                 <ul className='flex space-x-10' >
